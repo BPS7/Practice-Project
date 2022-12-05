d6d17d0a28433ce8a7885a901e66b907df2de9c1
Bhanu Pratap <bhanuraja786@gmail.com>
2022-12-05 11:50:05 +0530

    mixed

---
+++ bhanu/src/test/java/Practice/TextTEst.java
@@ -0,0 +1,175 @@
+package Practice;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class TextTEst  {
+	JavascriptExecutor js;
+	 WebDriverWait wait;
+	 WebDriver driver;
+	 ExtentReports extent;
+	 private static 	Logger log = LogManager.getLogger(TextTEst.class);
+	@BeforeTest
+	public void intiallize_browser() {
+		 		 
+	 System.setProperty("webdriver.chrome.driver","C:\\Users\\hp\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");
+	 driver = new ChromeDriver();	
+	 driver.manage().window().maximize();
+	 
+	 }
+	 @BeforeTest
+		public void report() {
+			 String path = System.getProperty("user.dir")+"\\report\\index";
+				ExtentSparkReporter report = new ExtentSparkReporter(path);
+				report.config().setReportName("Web automation results");
+				report.config().setDocumentTitle("Test result");
+			    extent = new ExtentReports();
+				extent.attachReporter(report);
+				extent.setSystemInfo("Tester", "Bhanu pratap");
+		}
+	@Test
+	public void test() throws InterruptedException, MalformedURLException, IOException {
+		extent.createTest("Practice");
+		log.info("opening url");
+	 driver.get("https://demoqa.com/elements");
+	 wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	 js = (JavascriptExecutor) driver;
+	 js.executeScript("window.scrollBy(0,300)");
+	 //windowhandles
+	 driver.findElement(By.xpath("(//div[@class='icon'])[1]"));
+	 driver.findElement(By.xpath("//span[text()='Link']")).click();
+	 driver.findElement(By.xpath("//a[@id='simpleLink']")).click();
+	 Set<String> window = driver.getWindowHandles();
+	            Iterator<String> id = window.iterator();
+	            String parentID = id.next();
+	            String childID = id.next();
+	            System.out.println(driver.switchTo().window(childID).getCurrentUrl());
+	            driver.close();
+	            driver.switchTo().window(parentID);
+	            driver.findElement(By.cssSelector("a[id='created']")).click();
+	            Thread.sleep(2000);
+	            System.out.println(driver.findElement(By.cssSelector("p[id='linkResponse']")).getText());
+	       	    js.executeScript("window.scrollBy(0,500)");
+	       	//broken link
+	       	 log.info("checking broken link");
+	            driver.findElement(By.xpath("//span[text()='Broken Links - Images']")).click();
+	            Thread.sleep(2000);
+	            js.executeScript("window.scrollBy(0,300)");
+	            WebElement link = driver.findElement(By.xpath("//a[text()='Click Here for Broken Link']"));
+	            SoftAssert a =new SoftAssert();
+	  	          String url= link.getAttribute("href");
+	  	          System.out.println(url);
+	  	          HttpURLConnection   conn= (HttpURLConnection)new URL(url).openConnection();
+	  	          conn.setRequestMethod("HEAD");
+	  	          conn.connect();
+	  	          int respCode = conn.getResponseCode();
+	  	          System.out.println(respCode);    
+	  	          a.assertTrue(respCode<400, "The link with Text"+link.getText()+" is broken with code" +respCode);        
+	  	 //broken images
+	  	        log.info("checking broken images");
+	  	        List<WebElement> images = driver.findElements(By.tagName("img"));
+	  	        System.out.println("Total number of Images on the Page are " + images.size());	
+	  	        for(WebElement image:images) {
+	  	        	String imageUrl = image.getAttribute("src");
+	  	        	URL urlimg = new URL(imageUrl);
+	  	            HttpURLConnection httpURLConnect=(HttpURLConnection)urlimg.openConnection();
+	  	            httpURLConnect.setConnectTimeout(5000);
+	  	            httpURLConnect.connect();
+	  	          if(httpURLConnect.getResponseCode()>=400)
+	              {
+	              	System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage() + "is a broken link");
+	              }    
+	              else{
+	                  System.out.println("HTTP STATUS - " + httpURLConnect.getResponseMessage());
+	              } 
+	  	          
+	  	        try {
+	                boolean imageDisplayed = (Boolean) ((JavascriptExecutor) driver).executeScript("return (typeof arguments[0].naturalWidth !=\"undefined\" && arguments[0].naturalWidth > 0);", image);
+	                if (imageDisplayed) {
+	                    System.out.println("DISPLAY - OK");
+	                }
+	                else {
+	                	System.out.println(imageUrl);
+	                     System.out.println("DISPLAY - BROKEN");
+	                }
+	            } 
+	            catch (Exception e) {
+	            	System.out.println("Error Occured");
+	            }
+	  	        }
+ 
+      //Buttons              	 
+	   driver.findElement(By.xpath("//span[text()='Buttons']")).click();
+	   Thread.sleep(2000);
+	   Actions cursor = new Actions(driver);
+	   cursor.moveToElement(driver.findElement(By.id("doubleClickBtn"))).doubleClick().build().perform();
+	   Thread.sleep(2000);
+	   System.out.println(driver.findElement(By.id("doubleClickMessage")).getText());
+	   cursor.moveToElement(driver.findElement(By.id("rightClickBtn"))).contextClick().build().perform();
+	   Thread.sleep(2000);
+	   System.out.println(driver.findElement(By.id("rightClickMessage")).getText());
+	   Thread.sleep(2000);
+	   //Dyanamic Properties
+	  // js.executeScript("window.scrollBy(0,450)");
+	   WebElement scroll = driver.findElement(By.xpath("//span[text()='Dynamic Properties']"));
+	   js.executeScript("arguments[0].scrollIntoView(true)", scroll);
+	   Thread.sleep(2000);
+	   driver.findElement(By.xpath("//span[text()='Dynamic Properties']")).click();
+	    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("enableAfter"))));
+	   System.out.println(driver.findElement(By.id("enableAfter")).isEnabled());
+	    System.out.println(driver.findElement(By.id("colorChange")).getText());
+	    extent.flush();
+	}
+	
+	@AfterMethod
+	public void screenshotAndDeleteCookies(ITestResult testResult) throws IOException {
+		//Taking screenshot in case of failure
+		log.info("Taking Screenshot");
+		if(testResult.getStatus() == ITestResult.FAILURE){
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(scrFile, new File("screenshots\\" + testResult.getName() + "-" 
+					+ Arrays.toString(testResult.getParameters()) +  ".jpg"));	
+			}
+		driver.manage().deleteAllCookies();
+	
+
+	}
+	@AfterTest
+	public void tear_down() {
+		driver.quit();
+	    	
+
+	}
+}
